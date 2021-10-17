38de4ac24ddc57dc597557483ed7fe9d7e713ae1
Ludovic Jozeau <federandink@gmail.com>
2021-10-17 23:34:08 +0200

    check deps and explain --why

--- rebuildaur.cpp
+++ rebuildaur.cpp
@@ -2,51 +2,169 @@
 #include <alpmpp/Package.hpp>
 
 #include <algorithm>
-#include <fstream>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using namespace std::literals;
 
-int main()
+struct string_hash
 {
-  alpmpp::Handle alpmh("/", "/var/lib/pacman", "/etc/pacman.conf");
+  using hash_type = std::hash<std::string_view>;
+  using is_transparent = void;
 
-  alpmpp::Package pkg = alpmh.get_local_pkg("gazebo");
-  std::cout << pkg.get_name() << " version: " << pkg.get_version() << "\n";
-  std::cout << "nb deps: " << pkg.get_depends(alpmh).size() << "\n";
-  std::cout << "nb optdeps: " << pkg.get_optdepends(alpmh).size() << "\n";
-  // pkg = alpmh.get_pkg("opencv");
-  // std::cout << pkg.get_name() << " version: " << pkg.get_version() << "\n";
-  auto deps = pkg.get_depends(alpmh);
-  std::cout << "nb deps: " << deps.size() << "\n";
-  for (auto&& dep : deps)
+  std::size_t operator()(const char* str) const
   {
-    if (dep.is_valid())
-    {
-      std::cout << "  " << dep.get_name() << "\n";
-    }
-    else
-    {
-      std::cout << "  invalid dep!\n";
-    }
+    return hash_type{}(str);
   }
-  auto optdeps = pkg.get_optdepends(alpmh);
-  std::cout << "nb optdeps: " << optdeps.size() << "\n";
-  for (auto&& dep : optdeps)
+  std::size_t operator()(std::string_view str) const
   {
-    if (dep.is_valid())
+    return hash_type{}(str);
+  }
+  std::size_t operator()(std::string const& str) const
+  {
+    return hash_type{}(str);
+  }
+};
+
+class Dependecy_checker
+{
+  alpmpp::Handle& handle;
+
+  std::unordered_map<std::string, bool, string_hash, std::equal_to<>> seen;
+
+public:
+  explicit Dependecy_checker(alpmpp::Handle& handle) : handle(handle) {}
+
+  void reset()
+  {
+    seen.clear();
+  }
+
+  bool check_deps(alpmpp::Package& pkg)
+  {
+    std::string name{pkg.get_name()};
+    if (auto it = seen.find(name); it != std::end(seen))
     {
-      std::cout << "  " << dep.get_name();
+      return it->second;
+    }
+    if (!pkg.get_orig_db().empty())
+    {
+      return false;
+    }
+    auto deps = pkg.get_depends(handle);
+    bool rebuild = false;
+    seen[name] = rebuild;
+
+    for (auto&& dep : deps)
+    {
       if (dep.get_installdate() > pkg.get_builddate())
       {
-        std::cout << " newer than pkg!";
+        rebuild = true;
+      }
+      seen.at(name) = rebuild;
+      auto cdeps_ret = check_deps(dep);
+      rebuild = rebuild || cdeps_ret;
+    }
+
+    if (rebuild)
+    {
+      std::cout << pkg.get_name() << "\n";
+    }
+
+    return rebuild;
+  }
+
+  bool check_deps_explain(alpmpp::Package& pkg)
+  {
+    std::string name{pkg.get_name()};
+    if (auto it = seen.find(name); it != std::end(seen))
+    {
+      return it->second;
+    }
+    if (!pkg.get_orig_db().empty())
+    {
+      return false;
+    }
+    auto deps = pkg.get_depends(handle);
+    bool rebuild = false;
+    seen[name] = rebuild;
+
+    std::vector<std::string_view> why;
+    for (auto&& dep : deps)
+    {
+      if (dep.get_installdate() > pkg.get_builddate())
+      {
+        rebuild = true;
+        why.push_back(dep.get_name());
+      }
+      seen.at(name) = rebuild;
+      auto cdeps_ret = check_deps_explain(dep);
+      rebuild = rebuild || cdeps_ret;
+    }
+
+    if (rebuild)
+    {
+      std::cout << pkg.get_name() << " caused by: ";
+      if (why.empty())
+      {
+        std::cout << "^";
       }
       std::cout << "\n";
+      for (auto name : why)
+      {
+        std::cout << "  " << name << "\n";
+      }
+    }
+
+    return rebuild;
+  }
+};
+
+void usage(std::string_view exe_name)
+{
+  std::cerr << "usage: " << exe_name << " pkgname\n";
+  std::cerr << "options:\n";
+  std::cerr << "  --why  : explain why the packages need to be rebuilt\n";
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    usage(argv[0]);
+    std::exit(1);
+  }
+  bool explain = false;
+  if (argc > 2)
+  {
+    if (argv[2] == "--why"sv)
+    {
+      explain = true;
     }
     else
     {
-      std::cout << "  invalid optdep\n";
+      std::cerr << "unknown option " << argv[2] << "\n";
+      usage(argv[0]);
+      std::exit(1);
     }
+  }
+
+  alpmpp::Handle handle("/", "/var/lib/pacman", "/etc/pacman.conf");
+
+  alpmpp::Package pkg = handle.get_local_pkg(argv[1]);
+  if (!pkg.is_valid())
+  {
+    std::cerr << argv[1] << " is not a local package\n";
+    std::exit(2);
+  }
+  Dependecy_checker dcheck{handle};
+  if (explain)
+  {
+    dcheck.check_deps_explain(pkg);
+  }
+  else
+  {
+    dcheck.check_deps(pkg);
   }
 }
