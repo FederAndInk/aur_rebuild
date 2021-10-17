725b7ecd696a4c8eb222ef7810339d2e33df41b8
Ludovic Jozeau <federandink@gmail.com>
2021-10-17 17:34:01 +0200

    look for Package satisfying provide dep

--- rebuildaur.cpp
+++ rebuildaur.cpp
@@ -9,6 +9,91 @@ using namespace std::literals;
 namespace alpm
 {
 
+template <typename T>
+class Alpm_range
+{
+  alpm_list_t* list;
+  alpm_list_t  lend;
+
+public:
+  explicit(false) Alpm_range(alpm_list_t* list)
+    : list(list), lend{.data = nullptr, .prev = alpm_list_last(list), .next = nullptr}
+  {
+  }
+
+  class Iterator
+  {
+    alpm_list_t* list;
+    alpm_list_t* lend;
+
+  public:
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using pointer = value_type*;   // or also value_type*
+    using reference = value_type&; // or also value_type&
+
+    explicit Iterator(alpm_list_t* list, alpm_list_t* lend) : list{list}, lend{lend} {}
+
+    reference operator*() const
+    {
+      return *(this->operator->());
+    }
+
+    pointer operator->() const
+    {
+      return reinterpret_cast<pointer>(&list->data);
+    }
+
+    Iterator& operator++()
+    {
+      list = list->next;
+      return *this;
+    }
+
+    Iterator operator++(int) const
+    {
+      auto tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    Iterator& operator--()
+    {
+      list = list->prev;
+      return *this;
+    }
+
+    Iterator operator--(int) const
+    {
+      auto tmp = *this;
+      --(*this);
+      return tmp;
+    }
+
+    friend bool operator==(Iterator const& lhs, Iterator const& rhs) noexcept
+    {
+      return lhs.list == rhs.list || (lhs.lend == rhs.list && lhs.list == nullptr) ||
+             (lhs.list == rhs.lend && rhs.list == nullptr);
+    }
+
+    friend bool operator!=(Iterator const& lhs, Iterator const& rhs) noexcept
+    {
+      return !(lhs == rhs);
+    }
+  };
+
+  [[nodiscard]] Iterator begin()
+  {
+    return Iterator{list, &lend};
+  }
+
+  [[nodiscard]] Iterator end()
+  {
+    return Iterator{&lend, &lend};
+  }
+};
+
 template <typename T,
           typename Lt = T,
           typename Fn = decltype([](Lt const& lt) { return lt; })>
@@ -30,9 +115,27 @@ class Package
   alpm_pkg_t* pkg{nullptr};
 
 public:
-  Package(alpm_db_t* db, const char* name)
+  Package(alpm_db_t* db, std::string_view name)
   {
-    pkg = alpm_db_get_pkg(db, name);
+    pkg = alpm_db_get_pkg(db, name.data());
+    if (!is_valid())
+    {
+      auto* pkg_as_dep = alpm_dep_from_string(name.data());
+      auto  pkg_hash = pkg_as_dep->name_hash;
+      alpm_dep_free(pkg_as_dep);
+      auto* pkgs = alpm_db_get_pkgcache(db);
+      for (auto* p : Alpm_range<alpm_pkg_t*>{pkgs})
+      {
+        for (auto* dep : Alpm_range<alpm_depend_t*>{alpm_pkg_get_provides(p)})
+        {
+          if (pkg_hash == dep->name_hash && name == dep->name)
+          {
+            pkg = p;
+            return;
+          }
+        }
+      }
+    }
   }
 
   [[nodiscard]] bool is_valid() const
@@ -111,7 +214,7 @@ public:
    * @brief get a package from local database
    *
    */
-  Package get_pkg(char const* name) const
+  Package get_pkg(std::string_view name) const
   {
     return Package(alpm_get_localdb(handle), name);
   }
@@ -126,8 +229,8 @@ int main(int argc, char** argv)
   std::cout << pkg.get_name() << " version: " << pkg.get_version() << "\n";
   std::cout << "nb deps: " << pkg.get_depends().size() << "\n";
   std::cout << "nb optdeps: " << pkg.get_optdepends().size() << "\n";
-  pkg = alpmh.get_pkg("opencv");
-  std::cout << pkg.get_name() << " version: " << pkg.get_version() << "\n";
+  // pkg = alpmh.get_pkg("opencv");
+  // std::cout << pkg.get_name() << " version: " << pkg.get_version() << "\n";
   auto deps = pkg.get_depends();
   std::cout << "nb deps: " << deps.size() << "\n";
   for (auto&& dep : deps)
@@ -147,7 +250,12 @@ int main(int argc, char** argv)
   {
     if (dep.is_valid())
     {
-      std::cout << "  " << dep.get_name() << "\n";
+      std::cout << "  " << dep.get_name();
+      if (dep.get_installdate() > pkg.get_builddate())
+      {
+        std::cout << " newer than pkg!";
+      }
+      std::cout << "\n";
     }
     else
     {
